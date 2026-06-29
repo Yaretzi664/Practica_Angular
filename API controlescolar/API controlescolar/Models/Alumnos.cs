eb34d1f0c0fd50ef9bbfb80842167f1e72cad8ba
Yaretzi664 <yaretzirubiomartinez80@gmail.com>
2026-06-29 10:02:11 -0600

    Archivo de API

---
+++ API controlescolar/API controlescolar/Models/Alumnos.cs
@@ -0,0 +1,10 @@
+﻿namespace API_controlescolar.Models
+{
+    public class Alumnos
+    {
+        public string Matricula { get; set; }
+        public int IdCarrera { get; set; }
+        public int IdDatosP { get; set; }
+        public bool Status { get; set; }
+    }
+}
